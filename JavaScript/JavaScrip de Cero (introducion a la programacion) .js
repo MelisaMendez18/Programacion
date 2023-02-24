71f48c8fff754b0f528042d9f096ba81c76f3a5e
MelisaMendez18 <melisamendezrian@gmail.com>
2023-02-23 20:56:30 -0500

    continuacion de cursos

--- JavaScript/JavaScrip de Cero (introducion a la programacion) .js
+++ JavaScript/JavaScrip de Cero (introducion a la programacion) .js
@@ -94,3 +94,54 @@ switch (operación){
     default:
         console.log ("El resultado no es 0 ni 7")
 }
+
+//Funciones (Anteriormente) //
+
+// function saludar (nombre = "carlos"){
+//  return `¡Hola ${nombre}!`
+//}
+
+//saludar()
+//saludar("paola")
+
+//function Inventario (nombre){
+//    this.nombre = nombre;
+//    this.articulos = [];
+//}
+//Inventario.prototype.getNombre = function(){
+//    return this.nombre;
+//}
+//Inventario.prototype.add = function(articulo, cantidad){
+//    this.articulo[articulo]= cantidad;
+//}
+//Inventario.prototype.cantidad = function(articulo){
+//    return this.articulos[articulo]
+//}
+//let libros = new Inventario ('libros');
+//libros.getNombre()
+//libros.add("Aprendiendo JavaScript", 5);
+//libros.cantidad ('Aprendiendo JavaScript'); 
+
+class Inventario {
+    constructor(nombre){
+        this.nombre = nombre;
+        this.articulos = [];
+    }
+
+    getNombre(){
+        return this.nombre;
+    }
+
+    add(articulo, cantidad){
+        this.articulo[articulo]= cantidad;
+    }
+
+    cantidad(articulo){
+        return this.articulos[articulo]
+    }
+}
+
+let libros = new Inventario ('libros');
+libros.getNombre()
+libros.add("Aprendiendo JavaScript", 5);
+libros.cantidad ('Aprendiendo JavaScript'); 
---
+++ JavaScript/Metodos de Array.js
@@ -0,0 +1,43 @@
+const posts = [{
+    id:1,
+    title: 'Mi primer post',
+    imagine: 'https//img.com/1',
+    tags: ['javascript', 'webdevelopment'],
+},{
+    id:2,
+    title: 'Mi primer react',
+    imagine: 'https//img.com/2',
+    tags: ['javascript', 'webdevelopment', 'react'],
+},{
+    id:3,
+    title: 'Mi primer angular',
+    imagine: 'https//img.com/3',
+    tags: ['javascript', 'webdevelopment', 'angular'],
+}]
+
+
+// Find
+
+Posr.find(post => post.id == 1);
+
+//some
+ 
+posts.some(post => post.id === 1) //true
+
+//Includes
+
+posts.some(post => post.tags.includes ('vue')) //false
+
+//Every
+
+posts.every(post => post.tags.includes ('javascript')) //true
+
+//Map
+
+posts.map(post => post.title) 
+
+//Filter
+
+posts.filter(post => post.tags.includes ('angular'))
+
+//
